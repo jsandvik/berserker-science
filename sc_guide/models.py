d7bedb040bf1ddbecd4492d6b1e0b4fb83f0345f
Jeff Sandvik <jeff@jeffsandvik.com>
2019-04-08 17:01:52 -0700

    updates schema with character table and additional frame values

--- sc_guide/models.py
+++ sc_guide/models.py
@@ -1,11 +1,25 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 Base = declarative_base()
+
+class Character(Base):
+    __tablename__ = "characters"
+    character_id = Column(Integer, primary_key=True, autoincrement=True)
+
+    moves = relationship("Move", back_populates="character")
 
 class Move(Base):
     __tablename__ = "moves"
 
-    move_id = Column(Integer, primary_key=True)
-    notation = Column(String, nullable=False)
+    move_id = Column(Integer, primary_key=True, autoincrement=True)
+    character_id = Column(Integer, ForeignKey('characters.character_id'))
+    command = Column(String, nullable=False)
     impact_frames = Column(Integer, nullable=False)
+    block_frames = Column(Integer, nullable=False)
+    hit_frames = Column(Integer, nullable=False)
+    counter_frames = Column(Integer, nullable=False)
+    damage = Column(Integer, nullable=False)
+
+    character = relationship("Character", back_populates="moves")
